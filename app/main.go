fe8dfbce53679e85f9ea12c24b499a8c39ac5e69
anuj <anuj.dhakrey@shl.com>
2025-12-19 16:06:39 +0530

    CLI tests (cli-test-1766140599642)

--- app/main.go
+++ app/main.go
@@ -21,22 +21,32 @@ func main() {
 			log.Fatal(err)
 		}
 
-		header := make([]byte, 100)
+		databaseHeader := make([]byte, 100)
 
-		_, err = databaseFile.Read(header)
+		_, err = databaseFile.Read(databaseHeader)
 		if err != nil {
 			log.Fatal(err)
 		}
 
 		var pageSize uint16
-		if err := binary.Read(bytes.NewReader(header[16:18]), binary.BigEndian, &pageSize); err != nil {
+		if err := binary.Read(bytes.NewReader(databaseHeader[16:18]), binary.BigEndian, &pageSize); err != nil {
 			fmt.Println("Failed to read integer:", err)
 			return
 		}
+
+		firstPageHeader := make([]byte, 12)
+		_, err = databaseFile.Read(firstPageHeader)
+		if err != nil {
+			log.Fatal(err)
+		}
+		var tableCount uint16
+		binary.Read(bytes.NewReader(firstPageHeader[3:5]), binary.BigEndian, &tableCount)
+
 		// You can use print statements as follows for debugging, they'll be visible when running tests.
 		fmt.Fprintln(os.Stderr, "Logs from your program will appear here!")
 
 		fmt.Printf("database page size: %v", pageSize)
+		fmt.Printf("\nnumber of tables: %v\n", tableCount)
 	default:
 		fmt.Println("Unknown command", command)
 		os.Exit(1)
